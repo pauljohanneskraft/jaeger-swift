e3fe7d0842d1f177b26e82a78d4e25cf03d7cf0f
Paul Kraft <pauljohanneskraft@quickbirdstudios.com>
2020-07-04 15:24:05 +0200

    Remove platforms

--- Package.swift
+++ Package.swift
@@ -1,33 +1,25 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "Jaeger",
-    platforms: [
-        .iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)
-    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Jaeger",
             targets: ["Jaeger"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        // .package(url: "https://github.com/swift-extensions/shapes.git", from: "0.1.0")
+
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Jaeger",
             dependencies: []
         ),
         .testTarget(
             name: "JaegerTests",
-            dependencies: ["Jaeger"]),
+            dependencies: ["Jaeger"]
+        ),
     ]
 )
